128df1664698eb2730a53147e0e4fc8f7f33ae98
Roberto Cipollone <cipollone.rt@gmail.com>
2022-02-24 18:58:19 +0100

    Working sapientino controls now.

--- stage_controls/robot_control.py
+++ stage_controls/robot_control.py
@@ -1,43 +1,56 @@
 """Interface for robot controls."""
 
 from __future__ import absolute_import, division, print_function
+import os
+import sys
+
+# Import the specific "marrtino" controls
+assert "MARRTINO_APPS_HOME" in os.environ, (
+    "MARRTINO_APPS_HOME should be the path of https://bitbucket.org/iocchi/marrtino_apps/")
+marrtino_apps_path = os.environ["MARRTINO_APPS_HOME"] + "/program"
+sys.path.append(str(marrtino_apps_path))
+import robot_cmd_ros as robot
 
 
 class RobotControl(object):
     """Controller class for the robot."""
 
+    max_vel = 0.5
+    max_ang_vel = 1.0
+    dt = 0.2  # Control frequency
+
     def __init__(self):
         """Initializations."""
-        # notes max_vel 0.5
-        # notes vel_ang 1.0 rad/s
-        # TODO: from old repo; update these
-        #robot.begin()
-        #robot.setMaxSpeed(self._max_tv, self._max_rv)
-        #robot.enableObstacleAvoidance(True)
-        #os.system("rosparam set /gradientBasedNavigation/max_vel_x %.2f" %
-        #    self._max_tv)
-        #os.system("rosparam set /gradientBasedNavigation/max_vel_theta %.2f" %
-        #    self._max_rv)
+        robot.begin()
+        robot.setMaxSpeed(self.max_vel, self.max_ang_vel)
+        robot.enableObstacleAvoidance(True)
+        os.system(
+            "rosparam set /gradientBasedNavigation/max_vel_x %.2f" %
+            self.max_vel
+        )
+        os.system(
+            "rosparam set /gradientBasedNavigation/max_vel_theta %.2f" %
+            self.max_ang_vel
+        )
 
         print("Initialized")
 
     def set_velocity(self, vel):
         """Modify the linear velocity."""
-        # TODO: update below
-        print("Setting vel ", vel)
+        robot.set_speed(vel, 0, self.dt, stopend=False)
 
     def set_position(self, x, y):
         """Modify the position in the map."""
-        # TODO: update below
-        print("Setting x, y ", x, y)
+        state = self.get_state()
+        angle = state[2]
+        robot.stage_setpose(x, y, angle)
 
     def set_angle(self, angle):
         """Rotate the robot to a target angle.
 
         Assuming angle is in degrees.
         """
-        # TODO: update below
-        print("Setting angle", angle)
+        robot.turn(angle, ref="ABS")
 
     def set_pose(self, x, y, angle):
         """Modify the pose.
@@ -51,10 +64,8 @@ class RobotControl(object):
     def get_state(self):
         """Retrieve configuration of the robot.
 
-        A configuration is [x, y, theta, vel, persondetected?]
+        A configuration is [x, y, theta, vel, ?]
         """
-        # TODO: fill this and remove below
-        #p = robot.getRobotPose(frame='gt')
-        #v = robot.getRobotVel()
-        # person detected?
-        return [1, 2, 3, 4, 0]
+        pose = robot.get_robot_pose()  # TODO: should frame be gt?
+        vel = robot.get_robot_vel()    # Linear and angular
+        return pose + [vel[0]] + [0]
